d4284bdae9d7aa8476ee21ee30eee014cb903872
Barış Berke MALKOÇ <barisberkemalkoc@gmail.com>
2022-06-15 00:49:37 +0300

    cf-7 (blog-card-3)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import ImageSlider from "./image_slider/ImageSlider";
 import Header from "./Header/Header";
 import {SliderData} from "./image_slider/SliderData";
 import HomeBlog from "./HomeBlog/HomeBlog";
-
+import BlogCardContainer from "./BlogCardContainer/BlogCardContainer";
 function App() {
   return (
     <div className="App">
         <Header/>
-        <ImageSlider slides={SliderData}></ImageSlider>
-        <HomeBlog></HomeBlog>
+      <ImageSlider slides={SliderData}></ImageSlider>
+        <HomeBlog/>
+        <BlogCardContainer/>
     </div>
   );
 }
--- src/BlogCardContainer/BlogCardContainer.jsx
+++ src/BlogCardContainer/BlogCardContainer.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import {BlogCardContainerData} from "./BlogCardContainerData";
+import "./BlogCardContainer.scss"
 
 function BlogCardContainer(props) {
-    return (
-        <div className={"blog-card-container"}>
-            <h3 className={"section-title"}>CAPTAIN FARGO BLOG</h3>
+  return (
+
+    <div className={"blog-container"}>
+        <h3 className={"section-title"}>CAPTAIN FARGO BLOG</h3>
+        <div className={"blog-card-row"}>
+          {BlogCardContainerData.map((blogCard, index) => {
+            return (
+              <div className={"blog-card"}>
+                <img className={"blog-card-image"} src={blogCard.image}/>
+                <h3>{blogCard.title}</h3>
+                <p>{blogCard.desc}</p>
+
+              </div>
+            )
+          })}
+
         </div>
-    );
+
+
+      <div className={"text-right"}>
+        <li>BLOĞUMUZA GÖZ ATIN</li>
+        <img src={"https://captainfargo.com/static/main/ic-right-arrow.179f968435bb.svg"}/>
+      </div>
+    </div>
+
+  );
 }
 
 export default BlogCardContainer;
