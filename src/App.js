c394adafe5e2541bc87b032698acceebed7728e4
ibrahim cagri koca <ibrahimcagri.koca@trendyol.com>
2022-06-14 23:37:35 +0300

    CF-4 | Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
 import ImageSlider from "./image_slider/ImageSlider";
+import Header from "./Header/Header";
 import {SliderData} from "./image_slider/SliderData";
 
 function App() {
   return (
     <div className="App">
+        <Header/>
         <ImageSlider slides={SliderData}></ImageSlider>
     </div>
   );
