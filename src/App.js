53f6bad7192f4267839225bfc4dce61b72c7df62
ibrahim cagri koca <ibrahimcagri.koca@trendyol.com>
2022-06-15 00:20:09 +0300

    CF-6 | Footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import ImageSlider from "./image_slider/ImageSlider";
 import Header from "./Header/Header";
 import {SliderData} from "./image_slider/SliderData";
 import HomeBlog from "./HomeBlog/HomeBlog";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <ImageSlider slides={SliderData}></ImageSlider>
-        <HomeBlog></HomeBlog>
+      <Header/>
+      <ImageSlider slides={SliderData}></ImageSlider>
+      <HomeBlog></HomeBlog>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './footer.scss'
+
+function Footer() {
+    return (
+      <div className={"footer"}>
+        <div className={"container"}>
+          <div className={"left-side"}>
+            <img src="https://captainfargo.com/static/main/logo_white.e7a528a00d67.png"/>
+            <p className={"address"}>Ömer Avni Mah. Beytül Malcı Sk. No: 10/9 Kabataş Beyoğlu/İstanbu</p>
+            <hr/>
+            <p className={"tel-no"}>
+              0850 304 85 99
+            </p>
+            <p className={"mail"}>
+              info@captainfargo.com
+            </p>
+          </div>
+          <div className={"right-side"}>
+            <ul>
+              <li>Mavi Yolculuk</li>
+              <li>Hakkımızda</li>
+              <li>SSS</li>
+              <li>Blog</li>
+              <li>Giriş Yap</li>
+              <li>Üye Ol</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Footer;
