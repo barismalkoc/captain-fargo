09a57cecdc28284c1b48c534fc2696b2d042c1b9
Barış Berke MALKOÇ <barisberkemalkoc@gmail.com>
2022-06-14 21:58:46 +0300

    cf-2 (image-slider)

---
+++ src/image_slider/ImageSlider.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+import "./ImageSlider.scss"
+function ImageSlider({slides}) {
+
+    const [current, setCurrent] = useState(0);
+    const length = slides.length;
+
+    useEffect(() => {
+        nextSlide();
+    }, []);
+
+    const nextSlide = () => {
+        if(current === length - 1) {
+            setCurrent(0);
+        }else{
+            setCurrent(current + 1);
+        }
+        setInterval(nextSlide,3000);
+    }
+
+    const prevSlide = () => {
+        if(current === 0) {
+            setCurrent(length - 1);
+        }else {
+            setCurrent(current -1);
+        }
+    }
+
+
+
+
+    return (
+        <div className={"slider"}>
+            <span className={"left-arrow"} onClick={prevSlide}></span>
+            <span className={"right-arrow"} onClick={nextSlide}></span>
+            {slides.map((slide,index) => {
+                return (
+                    <div key={index}>
+                        {index === current && (<img className={"slide-image"} src={slide.image} alt={"captain-fargo"}/>)}
+                    </div>
+
+                )
+            })}
+        </div>
+    );
+}
+
+export default ImageSlider;
---
+++ src/image_slider/SliderData.js
@@ -0,0 +1,12 @@
+export const SliderData = [
+    {
+        image : "https://images.pexels.com/photos/693521/pexels-photo-693521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+    },
+    {
+        image: "https://images.pexels.com/photos/870711/pexels-photo-870711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+        image: "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+]
