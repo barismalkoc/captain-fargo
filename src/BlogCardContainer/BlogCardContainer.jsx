ed3c2c92fa754dbccb2691a9012135c76c56bc92
Barış Berke MALKOÇ <barisberkemalkoc@gmail.com>
2022-06-14 23:44:54 +0300

    cf-5 (blog-card-1)

---
+++ src/BlogCardContainer/BlogCardContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BlogCardContainer(props) {
+    return (
+        <div className={"blog-card-container"}>
+            <h3 className={"section-title"}>CAPTAIN FARGO BLOG</h3>
+        </div>
+    );
+}
+
+export default BlogCardContainer;
